d81782ee68a283648709b9c069c45e5df1eb1145
Pranali <pranalilambat00@gmail.com>
2022-03-23 12:15:24 +0530

    [Pranali]Add:RideRepoUserId

--- CabInvoiceGenerator/CabInvoiceGenerator.cs
+++ CabInvoiceGenerator/CabInvoiceGenerator.cs
@@ -9,6 +9,8 @@ namespace Day30CabInvoiceGenerator
         const int COST_PER_KM = 10;
         const int COST_PER_MINUTE = 1;
         const int MIN_FAIR = 5;
+        RideRepository rideRepository = new RideRepository();
+
         List<Ride> rides = new List<Ride>();
         public double CalculateFare(double distance, int Time)
         {
@@ -20,25 +22,41 @@ namespace Day30CabInvoiceGenerator
             return MIN_FAIR;
         }
 
-        public void AddRide(double distance, int Time)
+        public void AddRide(string userId, double distance, int Time)
         {
-            rides.Add(new Ride()
+           var userRides= rideRepository.GetRideByUserId(userId);
+            if (userRides == null)
             {
-                distance = distance,
-                Time = Time
-            });
+                var userride = new List<Ride>();
+                userride.Add(new Ride()
+                {
+                    distance = distance,
+                    Time = Time
+                });
+                rideRepository.AddRideInRideRepo(userId, userride);
+            }
+            else
+            {
+                userRides.Add(new Ride()
+                {
+                    distance = distance,
+                    Time = Time
+                });
+                rideRepository.AddRideInRideRepo(userId, userRides);
+            }                                 
         }
-        public InvoiceSummary CalculateAggregate()
+        public InvoiceSummary CalculateAggregate(string userId)
         {
+            var userRides = rideRepository.GetRideByUserId(userId);
             double fair = 0;
-            foreach (Ride ride in rides)
+            foreach (Ride ride in userRides)
             {
                 fair += CalculateFare(ride.distance, ride.Time);
             }
             var summary = new InvoiceSummary()
             {
-                noOfRide = rides.Count,
-                AvgFare = fair / rides.Count,
+                noOfRide = userRides.Count,
+                AvgFare = fair / userRides.Count,
                 totalFare =(int) fair
             };
             return summary;
--- CabInvoiceGeneratorTest/UnitTest1.cs
+++ CabInvoiceGeneratorTest/UnitTest1.cs
@@ -27,26 +27,43 @@ namespace CabInvoiceGeneratorTest
         [Test]
         public void CalAggFairAndMulRides()
         {
-            cabInvoiceGenerator.AddRide(2, 5);
-            cabInvoiceGenerator.AddRide(12, 15);
-            var invoiceSummary = cabInvoiceGenerator.CalculateAggregate();
-            Assert.AreEqual(160, invoiceSummary.totalFare);
+            cabInvoiceGenerator.AddRide("pranali",2, 5);
+            cabInvoiceGenerator.AddRide("panu",12, 15);
+            cabInvoiceGenerator.AddRide("panu",12, 15);
+               
+            var invoiceSummary = cabInvoiceGenerator.CalculateAggregate("pranali");
+            Assert.AreEqual(25, invoiceSummary.totalFare);
         }
-        [Test]
+        [Test]//UC-3-ENHANCEDINVOICE
         public void CalAggFairAndMulRidesTestNoOfRide()
         {
-            cabInvoiceGenerator.AddRide(2, 5);
-            cabInvoiceGenerator.AddRide(12, 15);
-            var invoiceSummary = cabInvoiceGenerator.CalculateAggregate();
+            cabInvoiceGenerator.AddRide("parul",2, 5);
+            cabInvoiceGenerator.AddRide("helo",12, 15);
+            cabInvoiceGenerator.AddRide("helo", 12, 15);
+
+            var invoiceSummary = cabInvoiceGenerator.CalculateAggregate("helo");
             Assert.AreEqual(2, invoiceSummary.noOfRide);
         }
         [Test]
         public void AddMultipleRIdeToCheckAgrFare()
         {
-            cabInvoiceGenerator.AddRide(2, 5);
-            cabInvoiceGenerator.AddRide(12, 15);
-            var invoiceSummary = cabInvoiceGenerator.CalculateAggregate();
-            Assert.AreEqual(80, invoiceSummary.AvgFare);
+            cabInvoiceGenerator.AddRide("pp",2, 5);
+            cabInvoiceGenerator.AddRide("sd",12, 15);     
+
+            var invoiceSummary = cabInvoiceGenerator.CalculateAggregate("sd");
+            Assert.AreEqual(135, invoiceSummary.AvgFare);
         }
+        //UC-4-ENHANCEDSERVICE  
+        [Test]
+        public void AddMutipleRideForDifferentUsers()
+        {
+            cabInvoiceGenerator.AddRide("Pranali", 2, 5);
+            cabInvoiceGenerator.AddRide("Pranali", 12, 15);
+            cabInvoiceGenerator.AddRide("parul" ,12, 15);
+
+            var invoiceSummary = cabInvoiceGenerator.CalculateAggregate("parul");
+            Assert.AreEqual(135, invoiceSummary.AvgFare);
+        }
+
     }
 }
